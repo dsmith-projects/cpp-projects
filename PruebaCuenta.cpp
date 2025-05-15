db09fde743e5cae79346a68b9900a6e7fba0eb3e
dsmith-projects <dss.inbox@gmail.com>
2025-05-14 20:06:28 -0600

    modified the specification file to instantiate new objects using the class constructor function

--- PruebaCuenta.cpp
+++ PruebaCuenta.cpp
@@ -1,28 +1,19 @@
 // PruebaCuenta.cpp
 // Autor: Deitel&Deitel
 // Crear y manipular un objeto de la clase Cuenta
-
 #include <iostream>
-#include <string>  
 #include "Cuenta.h" // Definicion de la clase Cuenta
 
 using namespace std;
 
-int main()
-{
+int main() {
     // Crear un objeto de la clase Cuenta
-    Cuenta miCuenta; 
+    Cuenta cuenta1{"Juan Perez"}; // Inicializa el nombre de la cuenta
+    Cuenta cuenta2{"Maria Lopez"}; // Inicializa el nombre de la cuenta
 
-    cout << "El nombre inicial de la cuenta es: " << miCuenta.getNombre();
-
-    // Solicitar y leer el nombre
-    cout << "\nIngrese el nombre de la cuenta: ";
-    string nombre;
-    getline(cin, nombre);
-    miCuenta.setNombre(nombre);
-
-    // Muestrar el nombre almacenado en el objeto miCuenta
-    cout << "El nombre de la cuenta es: " << miCuenta.getNombre() << endl;
+    // Mostrar el nombre de la cuenta
+    cout << "El nombre de la cuenta1 es: " << cuenta1.getNombre() << endl;
+    cout << "El nombre de la cuenta2 es: " << cuenta2.getNombre() << endl;
 
     return 0; // Fin del programa
 }
