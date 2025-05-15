cb090b6ae8661af113a05e7a2d03cb895263f1fc
dsmith-projects <dss.inbox@gmail.com>
2025-05-14 19:46:55 -0600

    added constructor function

--- Cuenta.h
+++ Cuenta.h
@@ -1,5 +1,15 @@
+// Cuenta.h
+// Clase Cuenta
+#include <string>
+
 class Cuenta {
 public:
+    // Constructor por defecto
+    explicit Cuenta(std::string nombreCuenta) 
+        : nombre{nombreCuenta} {
+        // Cuerpo vacío
+    }
+
     // set
     void setNombre(std::string nombreCuenta) {
         nombre = nombreCuenta;
@@ -9,7 +19,6 @@ public:
     std::string getNombre() const { 
         return nombre; 
     }
-
 private:
     std::string nombre;
 };
