a6f8ec184859a28da5b4aa15342df60ce4c03011
dsmith-projects <dss.inbox@gmail.com>
2025-05-27 23:26:00 -0600

    changed folder structure

---
+++ Interes/Interes.cpp
@@ -0,0 +1,27 @@
+// Intereses.cpp
+// Cálculo de interés compuesto de una cuenta con ciclo for
+#include <iostream>
+#include <iomanip>
+#include <cmath> // función pow
+
+using namespace std;
+
+int main() {
+    
+    cout << fixed << setprecision(2); 
+
+    double principal{1000.00};
+    double tasa{0.05};
+
+    cout << "Capital inicial: " << principal << endl;
+    cout << "Tasa de interés: " << tasa << endl;
+    
+    cout << "\nYear" << setw(20) << "Importe del deposito" << endl;
+
+    for(unsigned int year{1}; year <= 10; year++){
+        double cantidad = principal * pow(1.0 + tasa, year);
+
+        cout << setw(4) << year << setw(20) << cantidad << endl;
+    }
+    
+}
